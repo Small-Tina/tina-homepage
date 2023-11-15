e18813985efe95b6541d95f48bf60a8738dda2ac
“SmallTina” <2190250079@qq.com>
2023-11-15 23:16:13 +0800

    Changes to be committed: 没错我又重构了 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.tsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+      <div className="text-slate-50">
+      <div>别急，在写了</div>
+      <iframe src="//player.bilibili.com/player.html?aid=80433022&bvid=BV1GJ411x7h7&cid=137649199&p=1" frameBorder={"0"} title="rickroll">
+        {' '}
+      </iframe>
+      <div>还是先看会rickroll吧</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
