8eb54648afd70d15b4760715084284c3ce487347
“SmallTina” <2190250079@qq.com>
2023-11-19 10:54:35 +0800

    Changes to be committed: 新增了tailwindcss 更改了样式排布 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/App.tsx 	new file:   src/components/Footer/index.tsx 	new file:   tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import './App.css';
 
 function App() {
-  
   return (
     <>
-      <div className="text-slate-50">
-      <div>别急，在写了</div>
-      <iframe src="//player.bilibili.com/player.html?aid=80433022&bvid=BV1GJ411x7h7&cid=137649199&p=1" frameBorder={"0"} title="rickroll">
-        {' '}
-      </iframe>
-      <div>还是先看会rickroll吧</div>
-    </div>
+      <div id="root">
+        <div>别急，在写了</div>
+        <div>
+          <iframe src="//player.bilibili.com/player.html?aid=80433022&bvid=BV1GJ411x7h7&cid=137649199&p=1" frameBorder={'0'} title="rickroll" className="mx-auto"></iframe>
+        </div>
+        <div>还是先看会rickroll吧</div>
+      </div>
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+export default function index() {
+  return (
+    <>
+      <div className="absolute bottom-0 flex">
+        <div id="MyName"></div>
+        <div id="TechStack"></div>
+        <div id="GitHub"></div>
+      </div>
+    </>
+  );
+}
