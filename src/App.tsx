be0dfc5f7a9c90a3b1af06017f8371a698fbe1db
“SmallTina” <2190250079@qq.com>
2023-12-02 19:52:21 +0800

    Changes： 增加了背景图片及其模糊

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import headPortrait from './assets/headPortrait.jpg';
 import {CSSTransition} from 'react-transition-group'
 
 function App() {
-
+const timeout:object = {
+  appear: 1500,
+  enter: 1300,
+  exit: 1500,
+  };
   return (
     <>
-      <CSSTransition in={true} appear={true} timeout={1500} classNames="headPortraitTransition">
-        <div className={"headPortraitContainer"}>
-          <div className={"headPortrait"}>
-            <img src={headPortrait} width={'200px'} alt={''} className="rounded-full headPortraitImg" />
-          </div>
-          <div className={"selfIntroduction"}>
-            <div className={"selfIntroductionText"}>
-              <div className="Tina">Tina</div>
-              <div className="">A Front-end developer</div>
+      <div className="background">
+        <CSSTransition in={true} appear={true} timeout={timeout} classNames="headPortraitTransition">
+          <div className={"headPortraitContainer"}>
+            <div className={"headPortrait"}>
+              <img src={headPortrait} width={'200px'} alt={''} className="rounded-full headPortraitImg" />
+            </div>
+            <div className={"selfIntroduction"}>
+              <div className={"selfIntroductionText"}>
+                <div className="text-5xl">Tina</div>
+                <div className="text-xl">A Front-end Developer</div>
+              </div>
             </div>
           </div>
-        </div>
-      </CSSTransition>
+        </CSSTransition>
+      </div>
     </>
   );
 }
