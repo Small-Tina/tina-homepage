dd4fbb2190708aa9d7254ea7e80083f2f325b012
“SmallTina” <2190250079@qq.com>
2024-01-01 09:33:08 +0800

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.tsx 	deleted:    src/assets/headPortrait.jpg 	new file:   src/assets/headPortrait.png 	modified:   src/assets/menu.svg 	modified:   tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
 import './App.css';
+import headPortrait from './assets/headPortrait.png';
+import menu from './assets/menu.svg';
 import { motion } from 'framer-motion';
-import headPortrait from './assets/headPortrait.jpg';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Modal } from 'antd';
 
-function App() {
-  const [list, setList] = useState(false);
-  function listClick() {
+const App: React.FC = () => {
+  const [list, setList] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
+  const listClick = () => {
     if (!list) {
       setList(true);
+      const menu: Element | null = document.querySelector('.menuBarNone');
+      menu!.className = 'menuBarDisplay';
     } else {
       setList(false);
+      const menu: Element | null = document.querySelector('.menuBarDisplay');
+      menu!.className = 'menuBarNone';
     }
-  }
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
+      <Modal title="提醒" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>网站将于农历新年正式完工</p>
+        <p>目前正在全力开发中</p>
+      </Modal>
       <div className="background">
         <div>
-          <div></div>
-          <button onClick={listClick}></button>
+          <motion.div className={'menuBarNone'}>
+            <button onClick={listClick}>
+              <motion.img className={'menuButton'} width={30} src={menu} alt="" whileHover={{ scale: 0.95 }} />
+            </button>
+          </motion.div>
         </div>
         <div className={'headPortraitContainer'}>
           <motion.div className="avatar" animate={{ scale: 1, rotate: 360 }}>
@@ -33,6 +54,6 @@ function App() {
       </div>
     </>
   );
-}
+};
 
 export default App;
